5819a1c1a645f5aefdcefa0e2406937f3a037534
vanil-1 <danilayanigin@gmail.com>
2025-09-19 15:47:35 +0300

    Commit 3.3

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -1,3 +1,3 @@
 surname, name, patronymic = map(str, input('ФИО: ').split())
-print(f'Инициалы: {surname[0] + name[0] + patronymic[0]}\n\
+print(f'Инициалы: {surname[0] + name[0] + patronymic[0]}.\n\
 Длина (символов): {len(surname + name + patronymic) + 2}')
